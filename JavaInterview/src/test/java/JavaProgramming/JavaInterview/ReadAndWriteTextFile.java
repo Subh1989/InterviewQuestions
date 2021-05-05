4578e325414c071b3fe2dd4bc981a3ef61ab70b4
Subh1989 <nsubhankar70@gmail.com>
2021-05-05 11:16:29 +0530

    added ReadAndWriteTextFile.java

---
+++ JavaInterview/src/test/java/JavaProgramming/JavaInterview/ReadAndWriteTextFile.java
@@ -0,0 +1,47 @@
+package JavaProgramming.JavaInterview;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadAndWriteTextFile {
+
+	public static void main(String[] args) throws IOException {
+		
+		ReadAndWriteTextFile.readText();
+		ReadAndWriteTextFile.writeText();
+
+	}
+
+	private static void writeText() throws IOException {
+		
+		FileWriter fw = new FileWriter(System.getProperty("user.dir")+"\\WriteText");
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("The Testing is completed");
+		bw.close();
+		FileReader fr = new FileReader(System.getProperty("user.dir")+"\\WriteText");
+		BufferedReader br = new BufferedReader(fr);
+		String s;
+		if((s=br.readLine())!=null)
+		{
+			System.out.println(s);
+		}
+		
+	}
+
+	private static void readText() throws IOException {
+		
+		FileReader fr = new FileReader(System.getProperty("user.dir")+"\\ReadText");
+		BufferedReader br = new BufferedReader(fr);
+		String s;
+		if((s=br.readLine())!=null)
+		{
+			System.out.println(s);
+		}
+		
+	}
+
+}
